c8f0cbc25aad8ef5d3f879979834e18c87818e42
HiCooper <294237781@qq.com>
2018-12-15 22:29:38 +0800

    查看评论页面 首页列表 发布页面

--- src/pemission.js
+++ src/pemission.js
@@ -1,20 +1,16 @@
 import router from './router'
 
 
-const whiteList = ['/login','/register'];// no redirect whitelist
+const whiteList = ['/login', '/register'];// no redirect whitelist
 
 router.beforeEach((to, from, next) => {
-    console.log('to:',to.path)
-    console.log('from:',from.path)
     const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
-    if (userInfo) { // determine if there has token
-        /* has token*/
+    if (userInfo) {
         if (to.path === '/login') {
             sessionStorage.removeItem('userInfo');
         }
         next()
     } else {
-        /* has no token*/
         if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
             next()
         } else {
